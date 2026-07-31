892ed02901391eddcf958367f4c5ba2e4792a360
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-07-31 23:19:39 +0330

    Merge pull request #2 from amirMshakouri/feature/contracts
    AUTH - Phase 2: Contracts

---
+++ CodeMate.Contracts/Auth/Responses/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeMate.Contracts.Auth.Responses
+{
+    public sealed class LoginResponse
+    {
+        public required string Token { get; set; } 
+
+        public  DateTimeOffset Expiration { get; set; }
+
+        public required string UserName { get; set; } 
+    }
+}

2e13d61e16bf79f809cee20622d8ca854961bcb0
amir shakouri <amir.shkr83@gmail.com>
2026-07-31 23:05:47 +0330

    feat(contracts): implement authentication contracts

---
+++ CodeMate.Contracts/Auth/Requests/RegisterRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeMate.Contracts.Auth.Requests
+{
+    public sealed class RegisterRequest
+    {
+        public required string UserName { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
+        public required string ConfirmPassword { get; set; }
+
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Bio { get; set; }
+    }
+}

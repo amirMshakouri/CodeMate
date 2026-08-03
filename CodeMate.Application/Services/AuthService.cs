e31c7fb8f4bf86f471ca9ffd3aa9b9ff3e336ebc
amir shakouri <amir.shkr83@gmail.com>
2026-08-03 22:49:06 +0330

    feat(application):add authservice.cs part1

--- CodeMate.Application/Services/AuthService.cs
+++ CodeMate.Application/Services/AuthService.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using CodeMate.Application.Common.Interfaces.Repositories;
+using CodeMate.Application.Common.Interfaces.Services;
+using CodeMate.Contracts.Auth.Requests;
+using CodeMate.Contracts.Auth.Responses;
 
-namespace CodeMate.Application.Services
+namespace CodeMate.Application.Services;
+
+public sealed class AuthService : IAuthService
 {
-    internal class AuthService
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public AuthService(
+        IUserRepository userRepository,
+        IMapper mapper)
     {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public Task<RegisterResponse> RegisterAsync(RegisterRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoginResponse> LoginAsync(LoginRequest request)
+    {
+        throw new NotImplementedException();
     }
 }

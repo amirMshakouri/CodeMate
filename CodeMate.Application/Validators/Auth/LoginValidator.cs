faa43b395473ce2f29f968baa26c814c2b736972
amir shakouri <amir.shkr83@gmail.com>
2026-08-03 22:34:53 +0330

    refactor(application): simplify login validator

--- CodeMate.Application/Validators/Auth/LoginValidator.cs
+++ CodeMate.Application/Validators/Auth/LoginValidator.cs
@@ -13,18 +13,6 @@ public sealed class LoginValidator : AbstractValidator<LoginRequest>
 
         RuleFor(x => x.Password)
             .NotEmpty()
-            .WithMessage("Password is required.")
-            .MinimumLength(8)
-            .WithMessage("Password must be at least 8 characters.")
-            .MaximumLength(128)
-            .WithMessage("Password must not exceed 128 characters.")
-            .Matches("[A-Z]")
-            .WithMessage("Password must contain at least one uppercase letter.")
-            .Matches("[a-z]")
-            .WithMessage("Password must contain at least one lowercase letter.")
-            .Matches("[0-9]")
-            .WithMessage("Password must contain at least one number.")
-            .Matches("[^a-zA-Z0-9]")
-            .WithMessage("Password must contain at least one special character.");
+            .WithMessage("Password is required.");
     }
 }

a6571f8010fadc72b1c9320279d65412ae2b6f3a
amir shakouri <amir.shkr83@gmail.com>
2026-08-03 22:27:18 +0330

    feat(application): add IUserRepository interface

--- CodeMate.Application/Common/Interfaces/Repositories/IUserRepository.cs
+++ CodeMate.Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using CodeMate.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CodeMate.Application.Common.Interfaces.Repositories
 {
-    internal class IUserRepository
+    public interface IUserRepository
     {
+        Task<User?> GetByIdAsync(Guid id);
+
+        Task<User?> GetByUserNameAsync(string userName);
+
+        Task<User?> GetByEmailAsync(string email);
+
+        Task<User?> GetByUserNameOrEmailAsync(string userNameOrEmail);
+
+        Task<bool> ExistsByUserNameAsync(string userName);
+
+        Task<bool> ExistsByEmailAsync(string email);
+
+        Task AddAsync(User user);
+
+        Task UpdateAsync(User user);
     }
 }

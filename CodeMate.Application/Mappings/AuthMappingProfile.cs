8337e79f9a49e97e7c7fb7f89178fe98a984e482
amir shakouri <amir.shkr83@gmail.com>
2026-08-03 22:40:00 +0330

    feat(application): add auth mapping profile

--- CodeMate.Application/Mappings/AuthMappingProfile.cs
+++ CodeMate.Application/Mappings/AuthMappingProfile.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using CodeMate.Contracts.Auth.Requests;
+using CodeMate.Contracts.Auth.Responses;
+using CodeMate.Domain.Entities;
 
-namespace CodeMate.Application.Mappings
+namespace CodeMate.Application.Mappings;
+
+public sealed class AuthMappingProfile : Profile
 {
-    internal class AuthMappingProfile
+    public AuthMappingProfile()
     {
+        CreateMap<RegisterRequest, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.SecondaryPassword, opt => opt.Ignore());
+
+        CreateMap<User, RegisterResponse>();
+
+        CreateMap<User, LoginResponse>()
+            .ForMember(dest => dest.Token, opt => opt.Ignore())
+            .ForMember(dest => dest.Expiration, opt => opt.Ignore());
     }
 }

739d0c08d2fb1daaab97764dfb7a861f3b5b4826
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-08-04 10:42:30 +0330

    Develop application auth (#10)
    * feat(application): add IAuthService interface
    
    * feat(application): add IUserRepository interface
    
    * feat(application): add register validator
    
    * refactor(application): simplify login validator
    
    * feat(application): add auth mapping profile
    
    * feat(application):add authservice.cs part1
    
    * finishing phase 3 (application)

---
+++ CodeMate.Application/Mappings/AuthMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CodeMate.Contracts.Auth.Requests;
+using CodeMate.Contracts.Auth.Responses;
+using CodeMate.Domain.Entities;
+
+namespace CodeMate.Application.Mappings;
+
+public sealed class AuthMappingProfile : Profile
+{
+    public AuthMappingProfile()
+    {
+        CreateMap<RegisterRequest, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.SecondaryPassword, opt => opt.Ignore());
+
+        CreateMap<User, RegisterResponse>();
+
+        CreateMap<User, LoginResponse>()
+            .ForMember(dest => dest.Token, opt => opt.Ignore())
+            .ForMember(dest => dest.Expiration, opt => opt.Ignore());
+    }
+}

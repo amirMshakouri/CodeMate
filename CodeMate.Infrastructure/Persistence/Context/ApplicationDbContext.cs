e2e3973d0b178c677a19014dcba9f4abbdbc2b9f
Atena Dolati <AtenaDolati2004@gmail.com>
2026-08-06 11:14:07 +0330

    feat(auth): implement application db context

--- CodeMate.Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ CodeMate.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CodeMate.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeMate.Infrastructure.Persistence.Context
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly);
+        }
     }
 }

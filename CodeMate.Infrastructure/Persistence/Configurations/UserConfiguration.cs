9ee3209fbe1076f69b016fc82ab63cb6f557b18f
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-08-07 11:46:59 +0330

    Feature/auth 15 configure user entity (#23)
    * feat(auth): implement application db context
    
    * feat(auth): configure user entity
    
    ---------
    
    Co-authored-by: Atena Dolati <AtenaDolati2004@gmail.com>

--- CodeMate.Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ CodeMate.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -1,10 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CodeMate.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CodeMate.Infrastructure.Persistence.Configurations
 {
-    internal class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.UserName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.SecondaryPassword)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.FullName)
+                .HasMaxLength(100);
+
+            builder.Property(x => x.PhoneNumber)
+                .HasMaxLength(11);
+
+            builder.Property(x => x.Bio)
+                .HasMaxLength(255);
+            
+            builder.HasIndex(x => x.UserName)
+                .IsUnique();
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+            
+            builder.Property(x => x.IsActive)
+                .IsRequired();
+        }
     }
 }

2abda5831b8104341e4851b3479ce6616201f7ee
amir shakouri <amir.shkr83@gmail.com>
2026-08-07 13:14:59 +0330

    fix(auth): address AUTH-17 code review feedback

--- CodeMate.Infrastructure/Authentication/Password/PasswordHasher.cs
+++ CodeMate.Infrastructure/Authentication/Password/PasswordHasher.cs
@@ -1,17 +1,16 @@
 ﻿using CodeMate.Application.Common.Interfaces.Security;
 
-namespace CodeMate.Infrastructure.Authentication.Password
-{
-    public class PasswordHasher : IPasswordHasher
-    {
-        public string Hash(string password)
-        {
-            return BCrypt.Net.BCrypt.HashPassword(password);
-        }
+namespace CodeMate.Infrastructure.Authentication.Password;
 
-        public bool Verify(string password, string passwordHash)
-        {
-            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
-        }
+public class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
 }
---
+++ CodeMate.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CodeMate.Application.Common.Interfaces.Security;
+using CodeMate.Infrastructure.Authentication.Password;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeMate.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+        return services;
+    }
+}

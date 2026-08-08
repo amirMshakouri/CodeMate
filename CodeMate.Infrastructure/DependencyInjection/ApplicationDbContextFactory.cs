9a09240cec4e91dd3f09beab4a53c08ba3663643
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-08-08 21:23:03 +0330

    feat: auth 19 - 20 (#29)

---
+++ CodeMate.Infrastructure/DependencyInjection/ApplicationDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using CodeMate.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CodeMate.Infrastructure.DependencyInjection
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? "Server=(localdb)\\MSSQLLocalDB;Database=CodeMateDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}

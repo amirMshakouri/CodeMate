128c3a5be4d1c0b165dab3542b692c48705d2dda
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-08-21 22:48:45 +0330

    feat: integrate user profile and skills (migration + DI) (#35)

--- CodeMate.Infrastructure/Persistence/Configurations/UserSkillConfiguration.cs
+++ CodeMate.Infrastructure/Persistence/Configurations/UserSkillConfiguration.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CodeMate.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CodeMate.Infrastructure.Persistence.Configurations
 {
-    internal class UserSkillConfiguration
+    public class UserSkillConfiguration : IEntityTypeConfiguration<UserSkill>
     {
+        public void Configure(EntityTypeBuilder<UserSkill> builder)
+        {
+            builder.ToTable("UserSkills");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Level)
+                .IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Skill)
+                .WithMany()
+                .HasForeignKey(x => x.SkillId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(x => new { x.UserId, x.SkillId })
+                .IsUnique();
+        }
     }
 }

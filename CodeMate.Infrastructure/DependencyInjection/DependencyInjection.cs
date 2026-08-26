6676b7f412c0ac11b3d4a18584b849056503ddd4
Farzanehmohammadiha <farzaneh.mohammadi144@gmail.com>
2026-08-26 22:22:39 +0330

    Complete project foundation

--- CodeMate.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ CodeMate.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -23,6 +23,7 @@ public static class DependencyInjection
                 configuration.GetConnectionString("DefaultConnection")));
 
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IProjectRepository, ProjectRepository>();
         services.AddScoped<ISkillRepository, SkillRepository>();
 
         services.Configure<JwtSettings>(

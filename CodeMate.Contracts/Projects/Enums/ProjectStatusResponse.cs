638d4034c9e69279b2afbf11ea06fd87de112028
Atena Dolati <AtenaDolati2004@gmail.com>
2026-08-28 15:41:04 +0330

    feat: add project query contracts

---
+++ CodeMate.Contracts/Projects/Enums/ProjectStatusResponse.cs
@@ -0,0 +1,9 @@
+namespace CodeMate.Contracts.Projects.Enums;
+
+public enum ProjectStatusResponse
+{
+    Draft = 1,
+    Pending = 2,
+    Active = 3,
+    Completed = 4
+}
--- CodeMate.Contracts/Projects/Responses/ProjectDetailsResponse.cs
+++ CodeMate.Contracts/Projects/Responses/ProjectDetailsResponse.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CodeMate.Contracts.Projects.Enums;
 
-namespace CodeMate.Contracts.Projects.Responses
+namespace CodeMate.Contracts.Projects.Responses;
+
+public class ProjectDetailsResponse
 {
-    internal class ProjectDetailsResponse
-    {
-    }
+    public Guid Id { get; set; }
+    public Guid OwnerId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public ProjectStatusResponse Status { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
 }

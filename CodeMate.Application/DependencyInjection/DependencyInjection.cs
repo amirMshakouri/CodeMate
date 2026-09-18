a92e192f62a4f4aa3921696f58f74ed366049d07
Amir Mohammad shakouri <145334025+amirMshakouri@users.noreply.github.com>
2026-09-18 11:45:52 +0330

    a tiny refactor (#51)

--- CodeMate.Application/DependencyInjection/DependencyInjection.cs
+++ CodeMate.Application/DependencyInjection/DependencyInjection.cs
@@ -26,6 +26,7 @@ public static class DependencyInjection
         services.AddScoped<IJoinRequestQueryService, JoinRequestQueryService>();
         services.AddScoped<IProjectDashboardService, ProjectDashboardService>();
         services.AddScoped<IMyDashboardService, MyDashboardService>();
+        services.AddScoped<ITaskQueryService, TaskQueryService>();
 
         return services;
     }
